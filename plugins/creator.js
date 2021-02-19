a08eb09cb68799b45395dd7be84e92fe7f1118ac
Ezequiel7u7xD <74691619+Ezequiel7u7xD@users.noreply.github.com>
2021-02-18 18:20:15 -0600

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  this.sendContact(m.chat, '+524641228001', 'Eze', m)
+  this.sendContact(m.chat, '524641228001', 'Eze', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
